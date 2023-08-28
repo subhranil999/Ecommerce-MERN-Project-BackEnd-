cb66854cbb96f016a60537041ea20c24a9b15204
subhranil999 <rijubm4@gmail.com>
2023-08-29 02:20:11 +0530

    BackEnd

---
+++ index.js
@@ -0,0 +1,146 @@
+const express = require('express');
+const server = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const session = require('express-session');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const cookieParser = require('cookie-parser');
+
+const productsRouter = require('./routes/Products');
+const categoriesRouter = require('./routes/Categories');
+const brandsRouter = require('./routes/Brands');
+const usersRouter = require('./routes/Users');
+const authRouter = require('./routes/Auth');
+const cartRouter = require('./routes/Cart');
+const ordersRouter = require('./routes/Order');
+const { User } = require('./model/User');
+const { isAuth, sanitizeUser, cookieExtractor } = require('./services/common');
+
+const SECRET_KEY = 'SECRET_KEY';
+// JWT options
+
+const opts = {};
+opts.jwtFromRequest = cookieExtractor;
+opts.secretOrKey = SECRET_KEY; // TODO: should not be in code;
+
+//middlewares
+
+server.use(express.static('build'))
+server.use(cookieParser());
+server.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false, 
+    saveUninitialized: false, 
+  })
+);
+server.use(passport.authenticate('session'));
+server.use(
+  cors({
+    exposedHeaders: ['X-Total-Count'],
+  })
+);
+server.use(express.json()); 
+
+server.use('/products', isAuth(), productsRouter.router);
+server.use('/categories', isAuth(), categoriesRouter.router);
+server.use('/brands', isAuth(), brandsRouter.router);
+server.use('/users', isAuth(), usersRouter.router);
+server.use('/auth', authRouter.router);
+server.use('/cart', isAuth(), cartRouter.router);
+server.use('/orders', isAuth(), ordersRouter.router);
+
+// Passport Strategies
+passport.use(
+  'local',
+  new LocalStrategy(
+    {usernameField:'email'},
+    async function (email, password, done) {
+    // by default passport uses username
+    try {
+      const user = await User.findOne({ email: email });
+      console.log(email, password, user);
+      if (!user) {
+        return done(null, false, { message: 'invalid credentials' }); // for safety
+      }
+      crypto.pbkdf2(
+        password,
+        user.salt,
+        310000,
+        32,
+        'sha256',
+        async function (err, hashedPassword) {
+          if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+            return done(null, false, { message: 'invalid credentials' });
+          }
+          const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
+          done(null, {id:user.id, role:user.role,token}); // this lines sends to serializer
+        }
+      );
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+
+passport.use(
+  'jwt',
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    console.log({ jwt_payload });
+    try {
+      const user = await User.findById(jwt_payload.id);
+      if (user) {
+        return done(null, sanitizeUser(user)); // this calls serializer
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+// this creates session variable req.user on being called from callbacks
+passport.serializeUser(function (user, cb) {
+  console.log('serialize', user);
+  process.nextTick(function () {
+    return cb(null, { id: user.id, role: user.role });
+  });
+});
+
+// this changes session variable req.user when called from authorized request
+
+passport.deserializeUser(function (user, cb) {
+  console.log('de-serialize', user);
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+main().catch((err) => console.log('Database not connected'));
+
+// async function main() {
+//   await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
+//   console.log('Database connected');
+// }
+
+async function main() {
+  await mongoose.connect('mongodb+srv://subhranil:riju1234@cluster0.hwymcbr.mongodb.net/ecommerce?retryWrites=true&w=majority');
+  console.log('Database connected');
+}
+
+// mongoose.connect('mongodb+srv://rijubm4:Riju1999@cluster0.hwymcbr.mongodb.net/ecommerce?retryWrites=true&w=majority')
+//     .then(()=>console.log('Database connected'))
+//     .catch((err)=> console.log('Database not connected'));
+
+
+
+server.listen(8080, () => {
+  console.log('Server started');
+});
